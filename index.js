f15844b65b1b380486dac5034495fe6e1389377a
Christine Schulte <christinerschulte@gmail.com>
2018-05-21 14:13:26 -0400

    Use ES6 class syntax

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = {
-    init(selectors){
+class App{
+    constructor(selectors){
         this.flicks = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
@@ -11,14 +11,14 @@ const app = {
                 ev.preventDefault()
                 this.handleSubmit(ev)
             })
-    },
+    }
 
     renderListItem(flick){
         const item = this.template.cloneNode(true)
         item.classList.remove('template')
         item.dataset.id = flick.id
         item.dataset.fav = flick.fav
-        flick.fav = 'false'
+        flick.fav = false
 
         item
             .querySelector('.flickName')
@@ -26,13 +26,34 @@ const app = {
 
         item
             .querySelector('button.delete')
-            .addEventListener('click', this.removeListItem.bind(this, flick))
+            .addEventListener(
+                'click', 
+                this.removeListItem.bind(this, flick)
+            )
 
         item
         .querySelector('button.favorite')
-        .addEventListener('click', this.favListItem.bind(this, flick))
+        .addEventListener(
+            'click', 
+            this.favListItem.bind(this, flick)
+        )
+
+        // item
+        // .querySelector('button.up')
+        // .addEventListener(
+        //     'click', 
+        //     this.moveItemUp.bind(this, flick)
+        // )
+
+        // item
+        // .querySelector('button.down')
+        // .addEventListener(
+        //     'click', 
+        //     this.moveItemDown.bind(this, flick)
+        // )
+
         return item
-    },
+    }
 
     removeListItem(flick, ev){
         const listItem = ev.target.closest('.flick')
@@ -40,15 +61,26 @@ const app = {
 
         const i = this.flicks.indexOf(flick)
         this.flicks.splice(i,1)
-    },
+    }
 
     favListItem(flick, ev){
         const listItem = ev.target
         listItem.style.backgroundColor = 'gold'
 
-        flick.fav = 'true'
+        flick.fav = true
         
-    },
+    }
+
+    moveItemUp(flick, ev){
+        const listItem = ev.target.closest('.flick')
+        const i = ev.indexOf(listItem) 
+        
+    }
+
+    moveItemDown(flick, ev){
+        const listItem = ev.target.closest('.flick')
+        const i = this.flicks.indexOf(flick)        
+    }
 
     handleSubmit(ev){
         const f = ev.target
@@ -62,11 +94,11 @@ const app = {
         const item = this.renderListItem(flick)
         this.list.insertBefore(item, this.list.firstChild)
         f.reset()
-    },
+    }
 
 }
 
-app.init({
+const app = new App({
     formSelector:'#flickForm',
     listSelector: '#flickList',
     templateSelector: '.flick.template',
